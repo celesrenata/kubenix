bec7701873de68df5ed1b52b5504ff75537e7bb4
Celes Renata <celes@celestium.life>
2026-01-26 00:30:47 +0000

    Add SR-IOV VF kernel patch

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,5 +1,14 @@
 final: prev: {
-  # Use kernel 6.18 directly - SR-IOV patch will be added separately if needed
-  linux_6_18 = prev.linux_6_18;
+  linux_6_18 = (prev.linuxPackages_6_18.kernel.override {
+    argsOverride = {
+      kernelPatches = [
+        {
+          name = "xe-mtl-sriov";
+          patch = ./. + "/../mtl-sriov.patch";
+        }
+      ];
+    };
+  });
+  
   linuxPackages_6_18 = prev.linuxPackagesFor final.linux_6_18;
 }
